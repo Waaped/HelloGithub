aa0998e86574192017a20449fb7a522aaf32033c
Waaped <rafaelvasquez.2015@gmail.com>
2018-01-14 01:41:14 -0800

    Create FirstTimeCallingaFunction.js

---
+++ FirstTimeCallingaFunction.js
@@ -0,0 +1,32 @@
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+// your code goes here
+
+function laugh(){
+    var laugh="hahahahahahahahahaha!";
+    return laugh;
+}
+
+console.log(laugh());
+
+
+//Here the emphasis is the Console.log, i was able to code the function correctly in the initial run
+//We then implemented the proper inside the console is a call to the laugh function
+//what i did was type // console.log(laugh);
+//I should have added "()"
+ //---------------------------------------------Correction was console.log(laugh());
+ 
+ 
+ //Here i modified the variable to see if changed the output, but i got the same result
+ //Expected error, but it seems variable names didnt change the output of the function,
+ //further expermination will be done as functions and material gets more advanced.
+ 
+ 
+ function laugh(){
+    var lol="hahahahahahahahahaha!";
+    return lol;
+}
+
+console.log(laugh());
