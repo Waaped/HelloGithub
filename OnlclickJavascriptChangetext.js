b8e95fb8d1fb272f2370fc331bb0296d5446d196
Waaped <rafaelvasquez.2015@gmail.com>
2018-01-14 00:37:12 -0800

    Create OnlclickJavascriptChangetext.js

---
+++ OnlclickJavascriptChangetext.js
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<h1 id="myHeader">Hello World!</h1>
+<button onclick="displayResult()">Change text</button>
+
+<script>
+function displayResult() {
+    document.getElementById("myHeader").innerHTML = "Have a nice day!";
+}
+</script>
+
+</body>
+</html>
+
+
+//Taken directly from w3schools, practiced and manually reiterated for muscle memory
+//Baconator.
