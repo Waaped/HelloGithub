26e108891f4f55f2e6f6a9b69ab127620044147a
Waaped <rafaelvasquez.2015@gmail.com>
2018-01-14 00:53:43 -0800

    INSOMNIA!

--- OnlclickJavascriptChangetext.js
+++ OnlclickJavascriptChangetext.js
@@ -16,3 +16,5 @@ function displayResult() {
 
 //Taken directly from w3schools, practiced and manually reiterated for muscle memory
 //Baconator.
+//The innerHTML property sets or returns the HTML content (inner HTML) of an element.
+// The innerHTMl SET OR RETURNS THE HTML CONTENT (INNER HTML) OF AN ELEMENT.
