e4068cb62d6e8d60adce9a2804be0aae190919b8
Waaped <rafaelvasquez.2015@gmail.com>
2018-01-21 21:01:51 -0800

    Create AnonymousFunctionAsVariable.js

---
+++ AnonymousFunctionAsVariable.js
@@ -0,0 +1,20 @@
+/*
+ * Programming Quiz: Laugh (5-4)
+ */
+
+var laugh = function(x){
+    ha="";
+    for(i=0;i<x;i++){
+    ha= ha + "ha";
+    }
+     return ha+"!";
+};
+
+
+/* finish the function expression */
+
+
+
+
+
+console.log(laugh(10));
