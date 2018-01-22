a96924a61659237a0bc71763aed0e50464f82a84
Waaped <rafaelvasquez.2015@gmail.com>
2018-01-21 22:37:43 -0800

    udgsd

--- Calling variables in Arrays.js
+++ Calling variables in Arrays.js
@@ -15,3 +15,21 @@ crew = [captain, second, pilot, companion, mercenary,
 mechanic];
 
 console.log(crew);
+
+
+
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
+
+prices[0]= 2;
+prices[2]=4;
+prices[6]=2;
+
+console.log(prices);
+
+//prints [ 2, 48.11, 4, 8.5, 9.99, 1, 2, 67 ]
