4ab53d3ee99b86bd88c3be8778536f95106330c6
Waaped <rafaelvasquez.2015@gmail.com>
2018-01-22 23:30:39 -0800

    Create FinalQuizUdacityForEach.js

---
+++ FinalQuizUdacityForEach.js
@@ -0,0 +1,20 @@
+/*
+ * Programming Quiz: Donuts Revisited (7-6)
+ */
+
+var donuts = [
+    { type: "Jelly", cost: 1.22 },
+    { type: "Chocolate", cost: 2.45 },
+    { type: "Cider", cost: 1.59 },
+    { type: "Boston Cream", cost: 5.99 }
+];
+
+// your code goes here
+// Write your code here
+
+donuts.forEach(function(donut){
+       console.log(donut.type+" donuts cost $"+donut.cost+" each");
+    
+});
+
+//Need to research this one a bit more :((, but did debug somethings alone
